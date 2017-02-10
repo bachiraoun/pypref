71eee0fcb3b61cce2511846060b547213a6dc43c
Bachir Aoun <bachir.aoun@e-aoun.com>
2017-02-10 17:41:24 -0600

    dynamic functionality added

--- Preferences.py
+++ Preferences.py
@@ -12,15 +12,14 @@ This example shows how to use pypref.
         pref = Preferences(filename="preferences_test.py")
         
         # create preferences dict example
-        pdict = {'preference 1': 1,
-                 12345: 'I am a number'}
+        pdict = {'preference 1': 1, 12345: 'I am a number'}
         
         # set preferences. This would automatically create preferences_test.py 
         # in your home directory. Go and check it.
         pref.set_preferences(pdict)
         
-        # let update the preferences. This would automatically update preferences_test.py
-        # file, you can verify that. 
+        # lets update the preferences. This would automatically update 
+        # preferences_test.py file, you can verify that. 
         pref.update_preferences({'preference 1': 2})
         
         # lets get some preferences. This would return the value of the preference if
@@ -31,6 +30,20 @@ This example shows how to use pypref.
         # working with paths in a windows systems or when a preference includes
         # especial characters. That's how to do it ...
         pref.update_preferences({'my path': " r'C:\Users\Me\Desktop' "})
+        
+        # Sometimes preferences to change dynamically or to be evaluated real time.
+        # This also can be done by using dynamic property. In this example password
+        # generator preference is set using uuid module. dynamic dictionary
+        # must include all modules name that must be imported upon evaluating
+        # a dynamic preference
+        pre = {'password generator': "str(uuid.uuid1())"}
+        dyn = {'password generator': ['uuid',]}
+        pref.update_preferences(preferences=pre, dynamic=dyn)
+        
+        # lets pull 'password generator' preferences twice and notice how
+        # passwords are different at every pull
+        print pref.get('password generator')
+        print pref.get('password generator')
         
         # those preferences can be accessed later. Let's simulate that by creating
         # another preferences instances which will automatically detect the 
@@ -97,16 +110,32 @@ class Preferences(object):
         self.__set_directory(directory=directory)
         self.__set_filename(filename=filename)
         # load existing or create file
+        self.__preferences = {}
+        self.__dynamic     = {}
         self.__load_or_create()
     
     def __getitem__(self, key):
-        return dict.__getitem__(self.__preferences, key)
+        pref = dict.__getitem__(self.__preferences, key)
+        dyn  = self.__dynamic.get(key, [])
+        # valuate preference
+        if len(dyn):
+            try:
+                for lib in dyn:
+                    locals()[lib] = __import__(lib)
+            except Exception as e:
+                raise Exception("Unable to import module '%s' for preference '%s' evaluation (e)"%(lib,pref,e))
+            try:
+                pref = eval(pref)
+            except Exception as e:
+                raise Exception("Unable to evaluate preference '%s' (%s)"%(key,e))
+        return pref
     
     def __set_directory(self, directory):
         if directory is None:
             directory = os.path.expanduser('~')
-        else:
-            assert os.path.isdir(directory), "Given directory '%s' is not an existing directory"%directory
+        # try to set directory if it doesn't exist
+        if not os.path.exists(directory):
+            os.makedirs( directory )
         # check if a directory is writable
         try:
             testfile = tempfile.TemporaryFile(dir = directory)
@@ -144,12 +173,20 @@ A valid filename must not contain especial characters or operating system separa
                 preferences = mod.preferences
             except Exception as e:
                 raise Exception("Existing file '%s' is not a pypref file (%s)"%(fullpath, e))
+            # try importing dynamic. Implemented started from version 2.0.0
+            try:
+                dynamic = mod.dynamic
+            except:
+                dynamic = {}
             # check preferences
             assert isinstance(preferences, dict), "Existing file '%s' is not a pypref file (%s)"%(fullpath, e)
+            assert isinstance(dynamic, dict),     "Existing file '%s' is not a pypref file (%s)"%(fullpath, e)
             self.__preferences = preferences
+            self.__dynamic     = dynamic
         else:
-            self.__dump_file(preferences = {})  
+            self.__dump_file(preferences = {}, dynamic = {})  
             self.__preferences = {}      
+            self.__dynamic     = {}
             
     def __get_normalized_string(self, s):
         stripped = s.strip()
@@ -161,7 +198,7 @@ A valid filename must not contain especial characters or operating system separa
                     s = "'%s'"%s
         return s
                          
-    def __dump_file(self,  preferences, temp=False):
+    def __dump_file(self, preferences, dynamic, temp=False):
         if temp:
             try:
                 fd = tempfile.NamedTemporaryFile(dir=tempfile._get_default_tempdir(), delete=True)
@@ -176,6 +213,8 @@ A valid filename must not contain especial characters or operating system separa
         # write preferences
         lines  = "# This file is an automatically generated pypref preferences file. \n\n" 
         lines += "__pypref_version__ = '%s' \n\n"%__version__ 
+        lines += "##########################################################################################\n"
+        lines += "###################################### PREFERENCES #######################################\n"
         lines += "preferences = {}" + "\n"
         for k, v in preferences.items():
             if isinstance(k, basestring):
@@ -183,6 +222,17 @@ A valid filename must not contain especial characters or operating system separa
             if isinstance(v, basestring):
                 v = self.__get_normalized_string(v)
             lines += "preferences[%s] = %s\n"%(k, v)
+        # write dynamic
+        lines += "\n"
+        lines += "##########################################################################################\n"
+        lines += "############################## DYNAMIC PREFERENCES MODULES ###############################\n"
+        lines += "dynamic = {}" + "\n"
+        for k, v in dynamic.items():
+            if isinstance(k, basestring):
+                k = self.__get_normalized_string(k)
+            if isinstance(v, basestring):
+                v = self.__get_normalized_string(v)
+            lines += "dynamic[%s] = %s\n"%(k, v)
         # write lines  
         try:     
             fd.write(lines) 
@@ -210,6 +260,11 @@ A valid filename must not contain especial characters or operating system separa
     def preferences(self):
         """Preferences dictionary copy."""
         return copy.deepcopy(self.__preferences)
+    
+    @property
+    def dynamic(self):
+        """Dynamic dictionary copy."""
+        return copy.deepcopy(self.__dynamic)
             
     def get(self, key, default=None):
         """
@@ -224,7 +279,21 @@ A valid filename must not contain especial characters or operating system separa
             #. value (object): The value of the given key or given default value is 
                key does not exist.
         """
-        return self.__preferences.get(key, default)
+        pref = self.__preferences.get(key, default)
+        dyn  = self.__dynamic.get(key, [])
+        # valuate preference
+        if len(dyn):
+            try:
+                for lib in dyn:
+                    locals()[lib] = __import__(lib)
+            except Exception as e:
+                raise Exception("Unable to import module '%s' for preference '%s' evaluation (e)"%(lib,pref,e))
+            try:
+                pref = eval(pref)
+            except Exception as e:
+                raise Exception("Unable to evaluate preference '%s'"%key)
+        # return
+        return pref
         
     def check_preferences(self, preferences):
         """
@@ -250,45 +319,98 @@ A valid filename must not contain especial characters or operating system separa
         """
         return True, ""
         
-    def set_preferences(self, preferences):
+    def set_preferences(self, preferences, dynamic=None):
         """
         Set preferences and write preferences file by erasing any existing one.
         
         :Parameters:
             #. preferences (dictionary): The preferences dictionary.
+            #. dynamic (None, dictionary): The dynamic dictionary. If None dynamic 
+               dictionary won't be updated.
         """
         flag, m = self.check_preferences(preferences)
         assert flag, m
         assert isinstance(preferences, dict), "preferences must be a dictionary"
-        # try dumping to temp file first
-        try:
-            self.__dump_file(preferences, temp=True)
-        except Exception as e:
-            raise Exception("Unable to dump temporary preferences file (%s)"%e)
-        # dump to file
-        try:
-            self.__dump_file(preferences, temp=False)
-        except Exception as e:
-            raise Exception("Unable to dump preferences file (%s). Preferences file can be corrupt, but in memory stored preferences are still available using and accessible using preferences property."%e)
+        if dynamic is None:
+            # try dumping to temp file first
+            try:
+                self.__dump_file(preferences=preferences, dynamic=self.__dynamic, temp=True)
+            except Exception as e:
+                raise Exception("Unable to dump temporary preferences file (%s)."%e)
+            # dump to file
+            try:
+                self.__dump_file(preferences=preferences, dynamic=self.__dynamic, temp=False)
+            except Exception as e:
+                raise Exception("Unable to dump preferences file (%s). Preferences file can be corrupt, but in memory stored preferences are still available and accessible using preferences property. Preferences are unchanged."%e)
+        else:
+            oldPreferences     = self.__preferences
+            self.__preferences = preferences
+            try:
+                self.set_dynamic(dynamic=dynamic)
+            except Exception as e:
+                self.__preferences = oldPreferences
+                raise Exception("Unable to set dynamic from set_preferences method. Preferences file can be corrupt, but in memory stored preferences are still available and accessible using preferences property. Preferences are unchanged."%e)
         # set preferences
         self.__preferences = preferences
     
-    def update_preferences(self, preferences):
+    def set_dynamic(self, dynamic):
         """
-        Update preferences and update preferences file.
+        Set dynamic and write preferences file by erasing any existing one.
+        
+        :Parameters:
+            #. dynamic (dictionary): The dynamic dictionary.
+        """
+        assert isinstance(dynamic, dict), "dynamic must be a dictionary"
+        D = {}
+        for k,v in dynamic.items():
+            if not self.__preferences.has_key(k):
+                warnings.warn("dynamic key '%s' is discarded as it's not a valid preference"%k)
+                continue
+            if v is not None:
+                assert isinstance(v, (list,set,tuple)), "dynamic dictionary values can be None or a list of importable modules"
+                assert len(set(v)) == len(v), "dynamic dictionary values are redundant for preference '%s'"%k
+                for m in v:
+                    assert isinstance(m, basestring), "dynamic dictionary values must be a list of strings"
+            else:
+                v = []
+            D[k] = tuple(v)
+        dynamic = D
+        # try dumping to temp file first
+        try:
+            self.__dump_file(preferences=self.__preferences, dynamic=dynamic, temp=True)
+        except Exception as e:
+            raise Exception("Unable to dump temporary preferences file. Preferences are unchanged. (%s)"%e)
+        # dump to file
+        try:
+            self.__dump_file(preferences=self.__preferences, dynamic=dynamic, temp=False)
+        except Exception as e:
+            raise Exception("Unable to dump preferences file (%s). Preferences file can be corrupt, but in memory stored preferences are still available and accessible using preferences property. Preferences are unchanged."%e)
+        # set dynamic preferences
+        self.__dynamic = dynamic
+        
+    def update_preferences(self, preferences, dynamic=None):
+        """
+        Update preferences and dynamic dictionaries and update preferences file.
         
         :Parameters:
             #. preferences (dictionary): The preferences dictionary.
+            #. dynamic (dictionary): The dynamic dictionary. If None is given, dynamic
+               dictionary won't get updated
         """
         flag, m = self.check_preferences(preferences)
         assert flag, m
         assert isinstance(preferences, dict), "preferences must be a dictionary"
         newPreferences = self.preferences
         newPreferences.update(preferences)
+        if dynamic is not None:
+            assert isinstance(dynamic, dict), "dynamic must be a dictionary"
+            dyn = self.dynamic
+            dyn.update(dynamic)
+            dynamic = dyn
         # set new preferences
-        self.set_preferences(newPreferences)
-        
-            
+        self.set_preferences(newPreferences, dynamic=dynamic)
+
+                
             
 class SinglePreferences(Preferences):
     """
