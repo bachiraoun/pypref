b0601837bff38265613063b7c27222f325300846
bachiraoun <bachir.aoun@e-aoun.com>
2019-02-16 11:39:35 -0600

    bug fixed

--- Preferences.py
+++ Preferences.py
@@ -5,32 +5,32 @@ This example shows how to use pypref.
 
 
 .. code-block:: python
-    
+
         from pypref import Preferences
-        
+
         #  create preferences instance
         pref = Preferences(filename="preferences_test.py")
-        
+
         # create preferences dict example
         pdict = {'preference 1': 1, 12345: 'I am a number'}
-        
-        # set preferences. This would automatically create preferences_test.py 
+
+        # set preferences. This would automatically create preferences_test.py
         # in your home directory. Go and check it.
         pref.set_preferences(pdict)
-        
-        # lets update the preferences. This would automatically update 
-        # preferences_test.py file, you can verify that. 
+
+        # lets update the preferences. This would automatically update
+        # preferences_test.py file, you can verify that.
         pref.update_preferences({'preference 1': 2})
-        
+
         # lets get some preferences. This would return the value of the preference if
         # it is defined or default value if it is not.
         print(pref.get('preference 1'))
-        
+
         # In some cases we must use raw strings. This is most likely needed when
         # working with paths in a windows systems or when a preference includes
         # especial characters. That's how to do it ...
         pref.update_preferences({'my path': " r'C:\\Users\\Me\\Desktop' "})
-        
+
         # Sometimes preferences to change dynamically or to be evaluated real time.
         # This also can be done by using dynamic property. In this example password
         # generator preference is set using uuid module. dynamic dictionary
@@ -39,23 +39,23 @@ This example shows how to use pypref.
         pre = {'password generator': "str(uuid.uuid1())"}
         dyn = {'password generator': ['uuid',]}
         pref.update_preferences(preferences=pre, dynamic=dyn)
-        
+
         # lets pull 'password generator' preferences twice and notice how
         # passwords are different at every pull
         print(pref.get('password generator'))
         print(pref.get('password generator'))
-        
+
         # those preferences can be accessed later. Let's simulate that by creating
-        # another preferences instances which will automatically detect the 
+        # another preferences instances which will automatically detect the
         # existance of a preferences file and connect to it
         newPref = Preferences(filename="preferences_test.py")
-        
+
         # let's print 'my path' preference
         print(newPref.get('my path'))
-        
-        
-        
-        
+
+
+
+
 Preferences main module:
 ========================
 """
@@ -83,67 +83,67 @@ else:
     bytes      = str
     long       = long
     basestring = basestring
-    
-    
+
+
 class Preferences(object):
     """
     This is pypref main preferences class definition. This class is used to create, load
     and update application's preferences dictionary in memory and in file.
     At initialization, a preferences dictionary will be pulled from the given directory
     and filename if existing. Otherwise, preferences file will be created and preferences
-    dictionary will be initialized to an empty dictionary. Preferences can be accessed 
+    dictionary will be initialized to an empty dictionary. Preferences can be accessed
     like any python dictionary by slicing a key preference [key] or by using get method.
-    
-    When used in a python application, it is advisable to use Preferences singleton 
+
+    When used in a python application, it is advisable to use Preferences singleton
     implementation and not Preferences itself.
     if no overloading is needed one can simply import the singleton as the following:
-    
+
     .. code-block:: python
-        
+
         from pypref import SinglePreferences as Preferences
-    
-    
-    
-    Recommended overloading implementation, this is how it could be done:    
-    
+
+
+
+    Recommended overloading implementation, this is how it could be done:
+
     .. code-block:: python
-        
+
         from pypref import SinglePreferences as PREF
-        
+
         class Preferences(PREF):
             # *args and **kwargs can be replace by fixed arguments
             def custom_init(self, *args, **kwargs):
                 # hereinafter any further instanciation can be coded
-                
-                
-                
-    In case overloading __init__ is needed, this is how it could be done:    
-    
+
+
+
+    In case overloading __init__ is needed, this is how it could be done:
+
     .. code-block:: python
-        
+
         from pypref import SinglePreferences as PREF
-        
+
         class Preferences(PREF):
             # custom_init will still be called in super(Preferences, self).__init__(*args, **kwargs)
             def __init__(self, *args, **kwargs):
                 if self._isInitialized: return
                 super(Preferences, self).__init__(*args, **kwargs)
                 # hereinafter any further instanciation can be coded
-                
-         
+
+
     :Parameters:
         #. directory (None, path): The directory where to create preferences file.
            If None is given, preferences will be create in user's home directory.
         #. filename (string): The preferences file name.
-        #. \*args (): This is used to send non-keyworded variable length argument 
+        #. \*args (): This is used to send non-keyworded variable length argument
            list to custom initialize. args will be parsed and used in custom_init method.
         #. \**kwargs (): This allows passing keyworded variable length of arguments to
-           custom_init method. kwargs can be anything other than 'directory' and 
+           custom_init method. kwargs can be anything other than 'directory' and
            'filename'.
-    
+
     **N.B. args and kwargs are not used by __init__ method but passed to custom_init
     at the end of initialization. custom_init method is an empty method that can be
-    overloaded**   
+    overloaded**
     """
     def __init__(self, directory=None, filename="preferences.py", *args, **kwargs):
         self.__set_directory(directory=directory)
@@ -154,8 +154,8 @@ class Preferences(object):
         self.__load_or_create()
         # custom initialize
         self.custom_init( *args, **kwargs )
-        
-    
+
+
     def __getitem__(self, key):
         pref = dict.__getitem__(self.__preferences, key)
         dyn  = self.__dynamic.get(key, [])
@@ -171,7 +171,7 @@ class Preferences(object):
             except Exception as e:
                 raise Exception("Unable to evaluate preference '%s' (%s)"%(key,e))
         return pref
-    
+
     def __set_directory(self, directory):
         if directory is None:
             directory = os.path.expanduser('~')
@@ -179,15 +179,15 @@ class Preferences(object):
         if not os.path.exists(directory):
             os.makedirs( directory )
         # check if a directory is writable
-        try:
+        try: 
             testfile = tempfile.TemporaryFile(dir = directory)
             testfile.close()
         except Exception as e:
             raise Exception("Given directory '%s' is not a writable directory."%directory)
         # set directory
         self.__directory = directory
-            
-    def __set_filename(self, filename): 
+
+    def __set_filename(self, filename):
         assert isinstance(filename, basestring), "filename must be a string, '%s' is given."%filename
         filename = str(filename)
         assert os.path.basename(filename) == filename, "Given filename '%s' is not valid. \
@@ -196,10 +196,15 @@ A valid filename must not contain especial characters or operating system separa
             filename += '.py'
             warnings.warn("'.py' appended to given filename '%s'"%filename)
         self.__filename = filename
-    
+
     def __load_or_create(self):
-        fullpath = self.fullpath
-        if os.path.isfile(fullpath):
+        fullpath     = self.fullpath
+        (path, name) = os.path.split(fullpath)
+        (name, ext)  = os.path.splitext(name)
+        exists       = os.path.isfile(fullpath) # this is not case sensitive !!!
+        if exists:
+            exists = name in os.listdir(path)
+        if exists:
             (path, name) = os.path.split(fullpath) # to use imp instead of importlib
             (name, ext)  = os.path.splitext(name)  # to use imp instead of importlib
             (file, filename, data) = imp.find_module(name, [path]) # to use imp instead of importlib
@@ -226,10 +231,10 @@ A valid filename must not contain especial characters or operating system separa
             self.__preferences = preferences
             self.__dynamic     = dynamic
         else:
-            self.__dump_file(preferences = {}, dynamic = {})  
-            self.__preferences = {}      
+            self.__dump_file(preferences = {}, dynamic = {})
+            self.__preferences = {}
             self.__dynamic     = {}
-            
+
     def __get_normalized_string(self, s):
         stripped = s.strip()
         if not stripped.startswith('"') and not stripped.endswith('"'):
@@ -239,7 +244,7 @@ A valid filename must not contain especial characters or operating system separa
                 else:
                     s = "'%s'"%s
         return s
-                         
+
     def __dump_file(self, preferences, dynamic, temp=False):
         if temp:
             try:
@@ -253,8 +258,8 @@ A valid filename must not contain especial characters or operating system separa
             except Exception as e:
                 raise Exception("Unable to open preferences file '%s."%self.fullpath)
         # write preferences
-        lines  = "# This file is an automatically generated pypref preferences file. \n\n" 
-        lines += "__pypref_version__ = '%s' \n\n"%__version__ 
+        lines  = "# This file is an automatically generated pypref preferences file. \n\n"
+        lines += "__pypref_version__ = '%s' \n\n"%__version__
         lines += "##########################################################################################\n"
         lines += "###################################### PREFERENCES #######################################\n"
         if isinstance(preferences, OrderedDict):
@@ -298,65 +303,65 @@ A valid filename must not contain especial characters or operating system separa
             if isinstance(v, basestring):
                 v = self.__get_normalized_string(v)
             lines += ("dynamic[%s]"%(k,)).ljust(maxLen) + " = %s\n"%(v,)
-        # write lines 
-        try:     
+        # write lines
+        try:
             fd.write( lines.encode('utf-8') )
         except Exception as e:
             raise Exception("Unable to write preferences to file '%s'. (%s)."%(self.fullpath, e))
         # close file
         fd.close()
-    
+
     @property
     def directory(self):
         """Preferences file directory."""
         return copy.deepcopy(self.__directory)
-    
+
     @property
     def filename(self):
         """Preferences file name."""
         return copy.deepcopy(self.__filename)
-    
+
     @property
     def fullpath(self):
         """Preferences file full path."""
         return os.path.join(self.__directory, self.__filename)
-    
+
     @property
     def preferences(self):
         """Preferences dictionary copy."""
         return copy.deepcopy(self.__preferences)
-    
+
     @property
     def dynamic(self):
         """Dynamic dictionary copy."""
         return copy.deepcopy(self.__dynamic)
-    
+
     def custom_init(self, *args, **kwargs):
         """
-        Custom initialize abstract method. This method will be called  at the end of 
-        initialzation. This method needs to be overloaded to custom initialize 
-        Preferences instances. 
-        
+        Custom initialize abstract method. This method will be called  at the end of
+        initialzation. This method needs to be overloaded to custom initialize
+        Preferences instances.
+
         :Parameters:
-            #. \*args (): This is used to send non-keyworded variable length argument 
-               list to custom initialize. 
+            #. \*args (): This is used to send non-keyworded variable length argument
+               list to custom initialize.
             #. \**kwargs (): This allows passing keyworded variable length of arguments to
-               custom_init method. kwargs can be anything other than 'directory' and 
+               custom_init method. kwargs can be anything other than 'directory' and
                'filename'.
         """
         pass
-             
+
     def get(self, key, default=None):
         """
-        Get the preferences value for the given key. 
+        Get the preferences value for the given key.
         If key is not available then returns then given default value.
-        
+
         :Parameters:
             #. key (object): The Key to be searched in the preferences.
             #. default (object): The Value to be returned in case key does not exist.
-        
+
         :Returns:
-            #. value (object): The value of the given key or given default value is 
+            #. value (object): The value of the given key or given default value is
                key does not exist.
         """
         pref = self.__preferences.get(key, default)
@@ -374,38 +379,38 @@ A valid filename must not contain especial characters or operating system separa
                 raise Exception("Unable to evaluate preference '%s'"%key)
         # return
         return pref
-        
+
     def check_preferences(self, preferences):
         """
-        This is an abstract method to be overloaded if needed. 
+        This is an abstract method to be overloaded if needed.
         All preferences setters such as set_preferences and update_preferences
-        call check_preferences prior to setting anything. This method returns 
-        a check flag and a message, if the flag is False, the message is raised 
+        call check_preferences prior to setting anything. This method returns
+        a check flag and a message, if the flag is False, the message is raised
         as an error like the following:
-        
+
         .. code-block:: python
-    
+
             flag, m = self.check_preferences(preferences)
             assert flag, m
-        
-        
+
+
         :Parameters:
             #. preferences (dictionary): The preferences dictionary.
-        
+
         :Returns:
             #. flag (boolean): The check flag.
             #. message (string): The error message to raise.
-            
+
         """
         return True, ""
-        
+
     def set_preferences(self, preferences, dynamic=None):
         """
         Set preferences and write preferences file by erasing any existing one.
-        
+
         :Parameters:
             #. preferences (dictionary): The preferences dictionary.
-            #. dynamic (None, dictionary): The dynamic dictionary. If None dynamic 
+            #. dynamic (None, dictionary): The dynamic dictionary. If None dynamic
                dictionary won't be updated.
         """
         flag, m = self.check_preferences(preferences)
@@ -433,11 +438,11 @@ A valid filename must not contain especial characters or operating system separa
                 raise Exception("Unable to set dynamic from set_preferences method. Preferences file can be corrupt, but in memory stored preferences are still available and accessible using preferences property. Preferences are unchanged."%e)
         # set preferences
         self.__preferences = preferences
-    
+
     def set_dynamic(self, dynamic):
         """
         Set dynamic and write preferences file by erasing any existing one.
-        
+
         :Parameters:
             #. dynamic (dictionary): The dynamic dictionary.
         """
@@ -468,11 +473,11 @@ A valid filename must not contain especial characters or operating system separa
             raise Exception("Unable to dump preferences file (%s). Preferences file can be corrupt, but in memory stored preferences are still available and accessible using preferences property. Preferences are unchanged."%e)
         # set dynamic preferences
         self.__dynamic = dynamic
-        
+
     def update_preferences(self, preferences, dynamic=None):
         """
         Update preferences and dynamic dictionaries and update preferences file.
-        
+
         :Parameters:
             #. preferences (dictionary): The preferences dictionary.
             #. dynamic (dictionary): The dynamic dictionary. If None is given, dynamic
@@ -491,8 +496,8 @@ A valid filename must not contain especial characters or operating system separa
         # set new preferences
         self.set_preferences(newPreferences, dynamic=dynamic)
 
-                
-            
+
+
 class SinglePreferences(Preferences):
     """
     This is singleton implementation of Preferences class.
@@ -503,21 +508,10 @@ class SinglePreferences(Preferences):
             cls.__thisInstance = super(SinglePreferences,cls).__new__(cls)
             cls.__thisInstance._isInitialized = False
         return cls.__thisInstance
-    
-    def __init__(self, *args, **kwargs):      
+
+    def __init__(self, *args, **kwargs):
         if (self._isInitialized): return
         # initialize
         super(SinglePreferences, self).__init__(*args, **kwargs)
         # update flag
         self._isInitialized = True
-        
-
-
-        
-                    
-            
-            
-            
-        
-        
-        
