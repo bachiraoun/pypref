ebcc7826b2baf340a8c02728d60cb17c5ecd3818
Eric Devolder <eric.devolder@mastercard.com>
2020-06-02 13:32:01 +0200

    adjusted python version for support of TemporaryDirectory

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -6,7 +6,7 @@ import tempfile
 # for Python 3.2+, there is a TemporaryDirectory class defined
 # a class substitute is proposed for lower versions.
 
-if sys.version_info>(3,2):
+if sys.version_info>=(3,2):
     from tempfile import TemporaryDirectory
 else:
     import shutil
