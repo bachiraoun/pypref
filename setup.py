c2057f0feff54cae932c9c87836f18bcaccfd789
SamHerts <sdherts@gmail.com>
2024-06-04 14:44:40 -0400

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except:
     from distutils.core import setup
 from distutils.util import convert_path
-import os, sys
+import sys
 
 # set package path and name
 PACKAGE_PATH = '.'
@@ -19,34 +19,33 @@ if major==2 and minor!=7:
 
 
 # automatically create MANIFEST.in
-commands = [# include MANIFEST.in
-            '# include this file, to ensure we can recreate source distributions',
-            'include MANIFEST.in'
-            # exclude all .log files
-            '\n# exclude all logs',
-            'global-exclude *.log',
-            # exclude all other non necessary files
-            '\n# exclude all other non necessary files ',
-            'global-exclude .project',
-            'global-exclude .pydevproject',
-            # exclude all of the subversion metadata
-            '\n# exclude all of the subversion metadata',
-            'global-exclude *.svn*',
-            'global-exclude .svn/*',
-            'global-exclude *.git*',
-            'global-exclude .git/*',
-            # include all LICENCE files
-            '\n# include all license files found',
-            'global-include ./*LICENSE.*',
-            # include all README files
-            '\n# include all readme files found',
-            'global-include ./*README.*',
-            'global-include ./*readme.*']
-with open('MANIFEST.in','w') as fd:
+commands = [  #  include MANIFEST.in
+    '# include this file, to ensure we can recreate source distributions',
+    'include MANIFEST.in'
+    # exclude all .log files
+    '\n# exclude all logs',
+    'global-exclude *.log',
+    # exclude all other non necessary files
+    '\n# exclude all other non necessary files ',
+    'global-exclude .project',
+    'global-exclude .pydevproject',
+    # exclude all of the subversion metadata
+    '\n# exclude all of the subversion metadata',
+    'global-exclude *.svn*',
+    'global-exclude .svn/*',
+    'global-exclude *.git*',
+    'global-exclude .git/*',
+    # include all LICENCE files
+    '\n# include all license files found',
+    'global-include ./*LICENSE.*',
+    # include all README files
+    '\n# include all readme files found',
+    'global-include ./*README.*',
+    'global-include ./*readme.*']
+with open('MANIFEST.in', 'w') as fd:
     for l in commands:
         fd.write(l)
         fd.write('\n')
-
 
 # declare classifiers
 CLASSIFIERS = """\
@@ -77,34 +76,28 @@ LONG_DESCRIPTION = ["PYthon PREFrences or pypref is a python implementation to c
                     "In addition, pypref allows creating dynamic preferences that will be evaluated real ",
                     "time. This becomes handy in plethora of applications for authentication, automatic ",
                     "key generation, etc. "]
-DESCRIPTION      = LONG_DESCRIPTION[:4]
+DESCRIPTION = LONG_DESCRIPTION[:4]
 
-
-## get package info
-PACKAGE_INFO={}
-if sys.version_info.major == 2:
-    execfile(convert_path( '__pkginfo__.py' ), PACKAGE_INFO)
-else:
-    exec(open(convert_path( '__pkginfo__.py' )).read(), PACKAGE_INFO)
-
-
+# get package info
+PACKAGE_INFO = {}
+exec(open(convert_path('__pkginfo__.py')).read(), PACKAGE_INFO)
 
 # create meta data
-metadata = dict(name = PACKAGE_NAME,
+metadata = dict(name=PACKAGE_NAME,
                 packages=[PACKAGE_NAME],
                 package_dir={PACKAGE_NAME: '.'},
-                version= PACKAGE_INFO['__version__'],
+                version=PACKAGE_INFO['__version__'],
                 author="Bachir AOUN",
                 author_email="bachir.aoun@e-aoun.com",
-                description = ''.join(DESCRIPTION),
-                long_description = ''.join(LONG_DESCRIPTION),
-                url = "http://bachiraoun.github.io/pypref/",
-                download_url = "https://github.com/bachiraoun/pypref",
-                license = 'GNU',
+                description=''.join(DESCRIPTION),
+                long_description=''.join(LONG_DESCRIPTION),
+                url="http://bachiraoun.github.io/pypref/",
+                download_url="https://github.com/bachiraoun/pypref",
+                license='GNU',
                 classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-                platforms = ["Windows", "Linux", "Mac OS-X", "Unix"],
+                platforms=["Windows", "Linux", "Mac OS-X", "Unix"],
                 test_suite="tests",
-)
+                )
 
 # setup
 setup(**metadata)
