8dc3a356c02c00661ec94ad78725b35d6d715fb3
SamHerts <sdherts@gmail.com>
2024-06-04 16:06:26 -0400

    reformat and update for setuputils

--- tests/test_string.py
+++ tests/test_string.py
@@ -2,17 +2,16 @@
 
 import unittest
 import warnings
-from pypref import Preferences
-from .fixtures import TemporaryDirectory
+from Preferences import Preferences
+from tempfile import TemporaryDirectory
 
 
 class TestPreferencesStrings(unittest.TestCase):
-
-    pref = {'string 1' : 'This is a string with interpolation{}',
-            'string 2' : 'With single quote',
-            'string 3' : "With double quote",
-            'string 4' : """Multi-line
-string""" }
+    pref = {'string 1': 'This is a string with interpolation{}',
+            'string 2': 'With single quote',
+            'string 3': "With double quote",
+            'string 4': """Multi-line
+string"""}
 
 
 class Test(unittest.TestCase):
@@ -20,7 +19,7 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.d = TemporaryDirectory()
         p = Preferences(directory=self.d.name)
-        p.set_preferences( TestPreferencesStrings.pref )
+        p.set_preferences(TestPreferencesStrings.pref)
 
     def tearDown(self):
         with warnings.catch_warnings():
@@ -30,15 +29,16 @@ class Test(unittest.TestCase):
     def test_interpolate_string(self):
         """test string interpolation"""
         p = Preferences(directory=self.d.name)
-        self.assertEqual(p.get('string 1',"{}").format('!'),
+        self.assertEqual(p.get('string 1', "{}").format('!'),
                          TestPreferencesStrings.pref['string 1'].format('!'))
 
     def test_various_string_formats(self):
         """test various string formats"""
         p = Preferences(directory=self.d.name)
 
-        for key,value in TestPreferencesStrings.pref.items():
-            self.assertEqual(p.get(key,""), value)
+        for key, value in TestPreferencesStrings.pref.items():
+            self.assertEqual(p.get(key, ""), value)
+
 
 if __name__ == '__main__':
     unittest.main(warnings=None)
