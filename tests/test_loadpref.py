076b30f83765641ea0dbf767cc260f16ad563127
SamHerts <sdherts@gmail.com>
2024-06-04 14:45:22 -0400

    reformat test_loadpref.py and use standard library import

--- tests/test_loadpref.py
+++ tests/test_loadpref.py
@@ -2,22 +2,24 @@
 
 import unittest
 from pypref import Preferences
-from .fixtures import TemporaryDirectory
+from tempfile import TemporaryDirectory
+
 
 class TestPreferencesMethods(unittest.TestCase):
 
     def setUp(self):
         self.d = TemporaryDirectory()
         p = Preferences(directory=self.d.name)
-        p.set_preferences({'preset' : True})
+        p.set_preferences({'preset': True})
 
     def test_loadexistingpref(self):
         """test persistence of preferences file"""
         p = Preferences(directory=self.d.name)
-        self.assertTrue(p.get('preset',False))
+        self.assertTrue(p.get('preset', False))
 
     def tearDown(self):
         self.d.cleanup()
+
 
 if __name__ == '__main__':
     unittest.main()
