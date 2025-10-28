c5291fc3a48be6fcec4440b5b2423a4e14536420
Bachir Aoun <bachir.aoun@e-aoun.com>
2025-10-28 09:39:01 -0500

    fixing importlib import

--- Preferences.py
+++ Preferences.py
@@ -78,6 +78,7 @@ import warnings
 
 try:
     import importlib
+    import importlib.util
     spec_from_loader = importlib.util.spec_from_loader
     module_from_spec = importlib.util.module_from_spec
     SourceFileLoader = importlib.machinery.SourceFileLoader
@@ -220,14 +221,16 @@ class Preferences(object):
         pref = dict.__getitem__(self.__preferences, key)
         dyn  = self.__dynamic.get(key, [])
         # valuate preference
+        _locals = {}
         if len(dyn):
             try:
                 for lib in dyn:
-                    locals()[lib] = __import__(lib)
+                    #locals()[lib] = __import__(lib)
+                    _locals[lib] = __import__(lib)
             except Exception as e:
                 raise Exception("Unable to import module '%s' for preference '%s' evaluation (e)"%(lib,pref,e))
             try:
-                pref = eval(pref)
+                pref = eval(pref, locals=_locals)
             except Exception as e:
                 raise Exception("Unable to evaluate preference '%s' (%s)"%(key,e))
         return pref
@@ -428,14 +431,16 @@ A valid filename must not contain especial characters or operating system separa
         pref = self.__preferences.get(key, default)
         dyn  = self.__dynamic.get(key, [])
         # valuate preference
+        _locals = {}
         if len(dyn):
             try:
                 for lib in dyn:
-                    locals()[lib] = __import__(lib)
+                    #locals()[lib] = __import__(lib)
+                    _locals[lib] = __import__(lib)
             except Exception as e:
                 raise Exception("Unable to import module '%s' for preference '%s' evaluation (e)"%(lib,pref,e))
             try:
-                pref = eval(pref)
+                pref = eval(pref, locals=_locals)
             except Exception as e:
                 raise Exception("Unable to evaluate preference '%s'"%key)
         # return
